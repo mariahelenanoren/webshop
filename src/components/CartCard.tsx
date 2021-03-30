462ff315bc5cb539df12f865aafaa86d1a6fff1b
mariahelenanoren <marialinder97@hotmail.com>
2021-03-30 10:37:46 +0200

    fixed styling of cartCard

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -1,6 +1,11 @@
-import { Box } from "@material-ui/core";
-import { CSSProperties } from "@material-ui/styles";
-import React from "react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import React, { CSSProperties } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 interface Props {
@@ -11,47 +16,83 @@ interface Props {
   onClick: React.MouseEventHandler<SVGSVGElement>;
 }
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    position: "absolute",
+    right: 0,
+    top: "0.5rem",
+    color: theme.palette.primary.main,
+    "&hover": {
+      color: "red",
+    },
+  },
+  lineHeight: {
+    lineHeight: 1.5,
+  },
+  buttonGroup: {
+    position: "absolute",
+    bottom: "0.5rem",
+    borderRadius: 0,
+    "&>.MuiButton-outlined:nth-of-type(1), &>.MuiButton-outlined:nth-of-type(3)": {
+      backgroundColor: theme.palette.secondary.main,
+    },
+    "&>.MuiButton-outlined": {
+      borderRadius: 0,
+      minWidth: "2.5rem",
+      padding: "0.1rem 0.3rem",
+    },
+  },
+}));
+
 export default function CartCard(props: Props) {
+  const classes = useStyles();
   return (
-    <Box style={box}>
+    <Box style={cardContainer}>
       <div style={imgContainer}>
         <img style={productImg} src={props.imageUrl} alt="product" />
       </div>
-      <div style={spaceBetween}>
-        <div className={"flex-direction alignStart"}>
-          <p style={itemSpacing}>{props.name}</p>
+      <div style={contentContainer}>
+        <div style={informationContainer}>
+          <Typography className={classes.lineHeight} variant="subtitle1">
+            {props.name}
+          </Typography>
+          <Typography className={classes.lineHeight} variant="subtitle2">
+            {props.price}&nbsp;kr
+          </Typography>
         </div>
-        <div className={"flex-direction alignEnd"}>
-          <p style={itemSpacing}>{props.price} kr</p>
-          <DeleteIcon style={{ cursor: "pointer" }} onClick={props.onClick} />
-        </div>
+        <DeleteIcon className={classes.icon} onClick={props.onClick} />
+        <ButtonGroup
+          className={classes.buttonGroup}
+          color="primary"
+          aria-label="outlined primary button group"
+        >
+          <Button>+</Button>
+          <Button>{props.quantity}</Button>
+          <Button>-</Button>
+        </ButtonGroup>
       </div>
     </Box>
   );
 }
 
-const spaceBetween: CSSProperties = {
+const cardContainer: CSSProperties = {
   display: "flex",
+  width: "100%",
+  height: "8rem",
+  alignItems: "flex-start",
+  flexDirection: "row",
+  marginBottom: "1rem",
+};
+
+const contentContainer: CSSProperties = {
+  position: "relative",
   width: "100%",
   height: "100%",
-  justifyContent: "space-between",
-  flexDirection: "row",
-  padding: "1.5rem",
+  padding: "0.5rem 0 0.5rem 1.5rem",
 };
 
-const itemSpacing: CSSProperties = {
-  margin: "0",
-};
-
-const box: CSSProperties = {
-  display: "flex",
-  height: "6.5rem",
-  width: "100%",
-  marginBottom: "1rem",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center",
-  backgroundColor: "white",
+const informationContainer: CSSProperties = {
+  marginRight: "2rem",
 };
 
 const productImg: CSSProperties = {
